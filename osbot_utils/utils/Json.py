0f25f46afdcd31726d33c8be7c22ef100131a14b
Dinis Cruz <dinis.cruz@owasp.org>
2021-01-03 16:47:44 +0000

    added rest of tests for Json

--- osbot_utils/utils/Json.py
+++ osbot_utils/utils/Json.py
@@ -3,7 +3,7 @@ import gzip
 import logging
 import os
 
-log = logging.getLogger()   # todo: start using this API for capturing error messages from methods bellow
+logger_json = logging.getLogger()   # todo: start using this API for capturing error messages from methods bellow
 
 from osbot_utils.utils.Files import file_exists, temp_file, file_create_gz, file_create, load_file_gz, file_contents
 
@@ -18,11 +18,7 @@ class Json:
                     return json.dumps(python_object, indent=indent)
                 return json.dumps(python_object)
             except:
-                log.exception('Error in load_json')
-
-    @staticmethod
-    def get_logger():
-        return log
+                logger_json.exception('Error in load_json')
 
     @staticmethod
     def load_file(path):
@@ -43,14 +39,12 @@ class Json:
 
     @staticmethod
     def load_file_gz(path):
-        if os.path.exists(path) is False:
-            return None
         data = load_file_gz(path)
         return json_loads(data)
 
     @staticmethod
     def load_file_gz_and_delete(path):
-        data = Json.load_json_gz(path)
+        data = json_load_file_gz(path)
         if data:
             os.remove(path)
         return data
@@ -66,7 +60,7 @@ class Json:
             try:
                 return json.loads(json_data)
             except:
-                log.exception('Error in load_json')
+                logger_json.exception('Error in load_json')
         return {}
 
     @staticmethod
@@ -87,15 +81,6 @@ class Json:
         json_data = json_dumps(python_object,indent=2, pretty=pretty)
         return file_create_gz(path=path, contents=json_data)
 
-        # with gzip.open(path, 'w') as fp:
-        #     fp.write(json_dump.encode())
-        # return path
-
-        # json_dump = json.dumps(data)
-        # with gzip.open(path, 'w') as fp:        # todo refactor to use Files methods
-        #     fp.write(json_dump.encode())
-        # return path
-
     @staticmethod
     def save_file_pretty_gz(python_object, path=None):
         return json_save_file_gz(python_object=python_object, path=path, pretty=True)
@@ -105,22 +90,19 @@ class Json:
     def json_save_tmp_file(python_object, pretty=True):
         return Json.save_file(python_object=python_object, pretty=pretty, path=None)
 
-    @staticmethod
-    def save_json_pretty(path, data):
-        return Json.save_file(path, data, pretty=True)
-
-json_dumps                  = Json.dumps
-json_format                 = Json.dumps
-json_load_file              = Json.load_file
-json_load_file_gz           = Json.load_file_gz
-json_load_file_and_delete   = Json.load_file_and_delete
-json_from_string            = Json.loads
-json_loads                  = Json.loads
-json_parse                  = Json.loads
-json_round_trip             = Json.round_trip
-json_save                   = Json.save_file
-json_save_file              = Json.save_file
-json_save_file_pretty       = Json.save_file_pretty
-json_save_file_gz           = Json.save_file_gz
-json_save_file_pretty_gz    = Json.save_file_pretty_gz
-json_save_tmp_file          = Json.json_save_tmp_file
+json_dumps                   = Json.dumps
+json_format                  = Json.dumps
+json_load_file               = Json.load_file
+json_load_file_and_delete    = Json.load_file_and_delete
+json_load_file_gz            = Json.load_file_gz
+json_load_file_gz_and_delete = Json.load_file_gz_and_delete
+json_from_string             = Json.loads
+json_loads                   = Json.loads
+json_parse                   = Json.loads
+json_round_trip              = Json.round_trip
+json_save                    = Json.save_file
+json_save_file               = Json.save_file
+json_save_file_pretty        = Json.save_file_pretty
+json_save_file_gz            = Json.save_file_gz
+json_save_file_pretty_gz     = Json.save_file_pretty_gz
+json_save_tmp_file           = Json.json_save_tmp_file
--- tests/utils/test_Json.py
+++ tests/utils/test_Json.py
@@ -1,39 +1,41 @@
-from pprint import pprint
+from datetime import datetime
 from unittest import TestCase
 
-from osbot_utils.utils.Files import file_exists, load_file_gz, file_lines_gz
+from osbot_utils.testing.Log_To_String import Log_To_String
+from osbot_utils.utils.Files import file_exists, load_file_gz, file_lines_gz, file_contents
 from osbot_utils.utils.Json import json_save_tmp_file, json_parse, json_loads, json_dumps, json_format, \
     json_load_file, json_load_file_and_delete, json_save_file_gz, json_save_file_pretty_gz, json_load_file_gz, \
-    json_round_trip, Json
+    json_round_trip, Json, logger_json, json_load_file_gz_and_delete, json_save_file_pretty, json_save_file
 
 
 class test_Json(TestCase):
 
+    def test_dumps__bad_object(self):
+        bad_obj          = { "date": datetime.now() }
+        expected_message = "TypeError: Object of type datetime is not JSON serializable"
+
+        with Log_To_String(logger_json) as log_to_string:
+            assert json_dumps(bad_obj) == None
+            assert expected_message in log_to_string.contents()
+
     def test_json_parse__json_format__json_dumps__json_loads(self):
-        # data = {'answer': 42 }
-        # assert json_dumps (data) == '{\n    "answer": 42\n}'
-        # assert json_format(data) == '{\n    "answer": 42\n}'
-        # assert json_parse (json_dumps(data)) == data
-        # assert json_loads (json_dumps(data)) == data
-        #
-        # assert json_dumps (None) is None
-        # assert json_format(None) is None
-        # assert json_parse (None) == {}
-        # assert json_loads (None) == {}
+        data = {'answer': 42 }
+        assert json_dumps (data) == '{\n    "answer": 42\n}'
+        assert json_format(data) == '{\n    "answer": 42\n}'
+        assert json_parse (json_dumps(data)) == data
+        assert json_loads (json_dumps(data)) == data
 
+        assert json_dumps (None) is None
+        assert json_format(None) is None
+        assert json_parse (None) == {}
+        assert json_loads (None) == {}
 
-        import logging
-        print()
-
-        from io import StringIO
-        stream = StringIO()
-        log = Json.get_logger()
-
-        handler = logging.StreamHandler(stream)
-        log.addHandler(handler)
-        print(stream.getvalue())
-        #print(json_loads("{ bad : json }") )
-        print(stream.getvalue())
+    def test_json_loads__bad_json(self):
+        bad_json         = "{ bad : json }"
+        expected_message = 'json.decoder.JSONDecodeError: Expecting property name enclosed in double quotes: line 1 column 3 (char 2)\n'
+        with Log_To_String(logger_json) as log_to_string:
+            assert json_loads(bad_json) == {}
+            assert expected_message in log_to_string.contents()
 
     def test_json_load_file__json_save_tmp_file(self):
         data = {'answer': 42 }
@@ -52,13 +54,17 @@ class test_Json(TestCase):
     def test_json_load_file_gz__json_save_file_pretty_gz(self):
         data = {'answer': 42}
         gz_file = json_save_file_pretty_gz(data)
-        json_load_file_gz(gz_file) == data
+        assert json_load_file_gz(gz_file) == data
+        assert file_exists(gz_file) is True
+        assert json_load_file_gz_and_delete(gz_file) == data
+        assert file_exists(gz_file) is False
 
+    def test_save_file_pretty(self):
+        data = {'answer': 42}
+        print()
+        assert file_contents(json_save_file(data))        == '{"answer": 42}'
+        assert file_contents(json_save_file_pretty(data)) == '{\n  "answer": 42\n}'
 
     def test_json_round_trip(self):
         data = {'answer': 42}
         assert json_round_trip(data) == data
-    # def test_json_load__json_save_tmp_file(self):
-    #     data = {'answer': 42}
-    #     json_file = json_save_tmp_file(data)
-    #     assert json_load(data=json_file) == data
