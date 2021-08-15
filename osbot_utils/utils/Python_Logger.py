21b5f85525d3b886a39e68af147b9888aaf3b282
Dinis Cruz <dinis.cruz@owasp.org>
2021-08-15 19:44:54 +0100

    Added util class Python_Logger

---
+++ osbot_utils/utils/Python_Logger.py
@@ -0,0 +1,177 @@
+import logging
+import sys
+from logging          import Logger
+from logging.handlers import MemoryHandler
+
+from osbot_utils.decorators.lists.group_by import group_by
+from osbot_utils.decorators.lists.index_by import index_by
+from osbot_utils.utils.Misc import random_string, obj_dict
+from osbot_utils.utils.Files import temp_file
+
+DEFAULT_LOG_LEVEL         = logging.DEBUG
+DEFAULT_LOG_FORMAT        = '%(asctime)s\t|\t%(name)s\t|\t%(levelname)s\t|\t%(message)s'
+MEMORY_LOGGER_CAPACITY    = 1024*10
+MEMORY_LOGGER_FLUSH_LEVEL = logging.ERROR
+
+class Python_Logger_Config:
+    def __init__(self):
+        self.elastic_host           = None
+        self.elastic_password       = None
+        self.elastic_port           = None
+        self.elastic_username       = None
+        #self.log_to_aws_s3          = False                     # todo
+        #self.log_to_aws_cloud_trail = False                     # todo
+        #self.log_to_aws_firehose    = False                     # todo
+        self.log_to_console         = False                     # todo
+        self.log_to_file            = False                     # todo
+        #self.log_to_elastic         = False                     # todo
+        self.log_to_memory          = False
+        self.path_logs              = None
+        self.log_format             = DEFAULT_LOG_FORMAT
+        self.log_level              = DEFAULT_LOG_LEVEL
+
+
+class Python_Logger:
+    config : Python_Logger_Config
+    logger : Logger
+    def __init__(self, logger_name= None, logger_config : Python_Logger_Config = None):
+        self.logger_name = logger_name or random_string(prefix="Python_Logger_")
+        self.set_config(logger_config)
+
+    def manager_get_loggers(self):
+        return Logger.manager.loggerDict
+
+    def manager_remove_logger(self):
+        logger_dict = Logger.manager.loggerDict
+        if self.logger_name in logger_dict:                 # need to do it manually here since Logger.manager doesn't seem to have a way to remove loggers
+            del logger_dict[self.logger_name]
+            return True
+        return False
+
+    def setup(self):
+        self.logger =  logging.getLogger(self.logger_name)
+        self.set_log_level()
+        self.add_handler_memory()
+        return self
+
+    # Setters
+    def set_config(self, config):
+        if type(config) is Python_Logger_Config:
+            self.config = config
+        else:
+            self.config = Python_Logger_Config()
+        return self.config
+
+    def set_log_level(self, level=None):
+        level = level or self.config.log_level
+        if self.logger:
+            self.logger.setLevel(level)
+            return True
+        return False
+
+    # Getters
+    def log_handlers(self):
+        if self.logger:
+            return self.logger.handlers
+        return []
+
+    def log_handler(self, handler_type):
+        for handler in self.log_handlers():
+            if type(handler) is handler_type:
+                return handler
+        return None
+
+    def log_handler_console(self):
+        return self.log_handler(logging.StreamHandler)
+
+    def log_handler_file(self):
+        return self.log_handler(logging.FileHandler)
+
+    def log_handler_memory(self):
+        return self.log_handler(MemoryHandler)
+
+    def log_formatter(self):
+        return logging.Formatter(self.config.log_format)
+
+    def log_level(self):
+        return self.config.log_level
+
+    # Actions
+
+    def add_console_logger(self):
+        self.config.log_to_console = True
+        return self.add_handler_console()
+
+    def add_memory_logger(self):
+        self.config.log_to_memory = True
+        return self.add_handler_memory()
+
+    def add_file_logger(self,path_log_file=None):
+        self.config.log_to_file = True
+        return self.add_handler_file(path_log_file=path_log_file)
+
+    # Handlers
+    def add_handler_console(self):
+        if self.logger and self.config.log_to_console:
+            handler = logging.StreamHandler(sys.stdout)
+            handler.setLevel(logging.DEBUG)
+            handler.setFormatter(self.log_formatter())
+            self.logger.addHandler(handler)
+            return True
+        return False
+
+    def add_handler_file(self, path_log_file=None):
+        if self.logger and self.config.log_to_file:
+            if path_log_file is None:
+                path_log_file = temp_file(extension='.log')
+            handler = logging.FileHandler(path_log_file)
+            handler.setLevel(self.log_level())
+            handler.setFormatter(self.log_formatter())
+            self.logger.addHandler(handler)
+            return True
+        return False
+
+    def add_handler_memory(self):
+        if self.logger and self.config.log_to_memory:
+            capacity       = MEMORY_LOGGER_CAPACITY
+            flush_level    = MEMORY_LOGGER_FLUSH_LEVEL
+            target         = None                       # we want the messages to only be kept in memory
+            memory_handler = MemoryHandler(capacity=capacity, flushLevel=flush_level, target=target,flushOnClose=True)
+            memory_handler.setLevel(self.log_level())
+            self.logger.addHandler(memory_handler)
+            return True
+        return False
+
+    # Utils
+    def memory_handler_exceptions(self):
+        return self.memory_handler_logs(index_by='levelname').get('EXCEPTIONS', {})
+
+    @index_by
+    @group_by
+    def memory_handler_logs(self):
+        logs           = []
+        memory_handler = self.log_handler_memory()
+        if memory_handler:
+            for log_record in memory_handler.buffer:
+                logs.append(obj_dict(log_record))
+        return logs
+
+    def memory_handler_messages(self):
+        return [log_entry.get('message') for log_entry in self.memory_handler_logs()]
+
+
+    # Logging methods
+
+    def debug    (self, msg='', *args, **kwargs): return self.log('debug'     , msg, *args, **kwargs)
+    def info     (self, msg='', *args, **kwargs): return self.log('info'      , msg, *args, **kwargs)
+    def warning  (self, msg='', *args, **kwargs): return self.log('warning'   , msg, *args, **kwargs)
+    def error    (self, msg='', *args, **kwargs): return self.log('error'     , msg, *args, **kwargs)
+    def exception(self, msg='', *args, **kwargs): return self.log('exception' , msg, *args, **kwargs)
+    def critical (self, msg='', *args, **kwargs): return self.log('critical'  , msg, *args, **kwargs)
+
+    def log(self, level, msg, *args, **kwargs):
+        if self.logger:
+            log_method = getattr(self.logger, level)
+            log_method(msg, *args, **kwargs)
+            return True
+        return False
---
+++ tests/utils/test_Python_Logger.py
@@ -0,0 +1,239 @@
+from _thread import RLock
+from datetime import datetime
+from io import TextIOWrapper, StringIO
+from logging import Logger, LogRecord
+from logging.handlers import MemoryHandler
+from unittest import TestCase
+from unittest.mock import patch
+
+import pytest
+
+from osbot_utils.utils.Json import json_load_file
+from osbot_utils.utils.Files import file_lines, file_exists, file_contents, file_delete
+from osbot_utils.utils.Python_Logger import Python_Logger, Python_Logger_Config, DEFAULT_LOG_LEVEL, \
+    MEMORY_LOGGER_CAPACITY, MEMORY_LOGGER_FLUSH_LEVEL
+from osbot_utils.utils.Misc import len_list, list_set, obj_dict, obj_items, size, list_set_dict
+
+
+class test_Python_Logger_Config(TestCase):
+    def setUp(self):
+        self.config = Python_Logger_Config()
+
+    def test__init__(self):
+        assert obj_dict(self.config) == {'elastic_host'     : None  ,
+                                         'elastic_password' : None  ,
+                                         'elastic_port'     : None  ,
+                                         'elastic_username' : None  ,
+                                         'log_level'        : 10    ,
+                                         'log_format'       : '%(asctime)s\t|\t%(name)s\t|\t%(levelname)s\t|\t%(message)s',
+                                         'log_to_console'   : False ,
+                                         'log_to_file'      : False ,
+                                         'log_to_memory'    : False ,
+                                         'path_logs'        : None  }
+
+
+class test_Python_Logger(TestCase):
+
+    def setUp(self):
+        self.logger = Python_Logger().setup()
+
+    def tearDown(self) -> None:
+        self.logger.manager_remove_logger()
+
+    def test__init__(self):
+        assert obj_items(self.logger.config) == obj_items(Python_Logger_Config())
+        assert self.logger.logger            is not None
+
+    # Helpers and Setup methods
+
+    def test_manager_get_loggers(self):
+        loggers = list_set(self.logger.manager_get_loggers())
+        assert len(loggers) > 5
+        assert self.logger.logger_name  in loggers
+        assert 'dotenv'                 in loggers
+        assert 'dotenv.main'            in loggers
+
+    def test_setup(self):                           # setup is called as part of the Unit Tests setUp() with default values
+        config = self.logger.config
+        logger = self.logger.logger
+
+        assert type(logger)         == Logger
+        assert logger.disabled      == False
+        assert logger.filters       == []
+        assert logger.level         == config.log_level
+        assert logger.propagate     == True
+        assert logger.handlers      == []
+        assert logger.name.startswith("Python_Logger") is True
+
+    @pytest.mark.skip
+    def test_get_logger(self):
+        logger = self.logger.logger()
+
+        logger.info('this is an info message')
+
+        assert len_list(file_lines(self.logger.logger_file)) == 1
+        log_data = json_load_file(self.logger.logger_file)
+        assert list_set(log_data) == ['@timestamp', 'ecs', 'log', 'log.level', 'message', 'process']
+        assert log_data.get('message') == 'this is an info message'
+
+    # Logging methods
+
+
+
+    @patch('sys.stdout', new_callable=StringIO)
+    def test_add_console_logger(self, sys_stdout):
+        assert self.logger.add_console_logger() is True
+        console_handler = self.logger.log_handler_console()
+        obj_data = obj_dict(console_handler)
+        assert obj_data == { '_name'        : None                      ,
+                              'filters'     : []                        ,
+                              'formatter'   : obj_data.get('formatter') ,
+                              'level'       : DEFAULT_LOG_LEVEL         ,
+                              'lock'        : obj_data.get('lock')      ,
+                              'stream'      : obj_data.get('stream')    }
+
+        debug_message = 'an debug message'
+        self.logger.debug(debug_message)
+        log_entry = sys_stdout.getvalue().split('\t|\t')
+        assert len(log_entry) == 4
+        assert str(datetime.now().minute) in log_entry[0]
+        assert self.logger.logger_name    == log_entry[1]
+        assert 'DEBUG'                    == log_entry[2]
+        assert f'{debug_message}\n'       == log_entry[3]
+
+    def test_add_file_logger(self):
+        assert self.logger.add_file_logger() is True
+        file_handler = self.logger.log_handler_file()
+        log_file = file_handler.baseFilename
+        obj_data = obj_dict(file_handler)
+        assert obj_data== {'_name'          : None                        ,
+                            'baseFilename'  : obj_data.get('baseFilename'),
+                            'delay'         : False                       ,
+                            'encoding'      : None                        ,
+                            'filters'       : []                          ,
+                            'formatter'     : obj_data.get('formatter')   ,
+                            'level'         : DEFAULT_LOG_LEVEL           ,
+                            'lock'          : obj_data.get('lock')        ,
+                            'mode'          : 'a'                         ,
+                            'stream'        : obj_data.get('stream')      }
+
+        assert file_exists(log_file)   is True
+        assert file_contents(log_file) == ''
+        assert type(obj_data.get('stream')) == TextIOWrapper
+
+
+        info_message = 'an info  message'
+        self.logger.info(info_message)
+
+        log_message_items = file_contents(log_file).split('\t|\t')
+        assert len(log_message_items) == 4
+        assert datetime.now().strftime('%Y-%m-%d %H:%M:%S') in log_message_items[0]
+        assert self.logger.logger_name                      == log_message_items[1]
+        assert 'INFO'                                       == log_message_items[2]
+        assert f'{info_message}\n'                          == log_message_items[3]
+
+        self.logger.info('an debug  message')
+        log_lines = list(file_lines(log_file))
+        assert len(log_lines) == 2
+        assert 'an debug  message\n' in log_lines.pop()
+        assert file_delete(log_file)
+
+
+    def test_add_memory_logger(self):
+        mem_handler : MemoryHandler
+        assert self.logger.config.log_to_memory is False
+        assert self.logger.add_handler_memory() is False
+        assert self.logger.add_memory_logger     () is True
+
+        mem_handler = self.logger.log_handler(MemoryHandler)
+        assert size(self.logger.log_handlers()) == 1
+        assert mem_handler                      is not None
+        obj_data = obj_dict(mem_handler)
+        assert type(obj_data['lock'])   is RLock
+
+        assert obj_data == {  '_name'       : None                      ,
+                              'buffer'      : []                        ,
+                              'capacity'    : MEMORY_LOGGER_CAPACITY    ,
+                              'filters'     : []                        ,
+                              'flushLevel'  : MEMORY_LOGGER_FLUSH_LEVEL ,
+                              'flushOnClose': True                      ,
+                              'formatter'   : None                      ,
+                              'level'       : DEFAULT_LOG_LEVEL         ,
+                              'lock'        : obj_data['lock'  ]        ,
+                              'target'      : None       }
+
+        assert mem_handler.buffer == []
+        self.logger.info('an info message')
+        assert size(mem_handler.buffer ) == 1
+        log_record = mem_handler.buffer.pop()
+        assert type(log_record) is LogRecord
+
+        assert list_set_dict(log_record) == [ 'args', 'created', 'exc_info', 'exc_text', 'filename',
+                                              'funcName', 'levelname', 'levelno', 'lineno', 'message',
+                                              'module', 'msecs', 'msg', 'name', 'pathname', 'process',
+                                              'processName', 'relativeCreated', 'stack_info', 'thread', 'threadName']
+
+        assert log_record.message == 'an info message'
+
+    def test_memory_handler_messages(self):
+        import requests
+        urllib3_logger = Python_Logger(logger_name='urllib3').setup()           # get urllib3 logger
+        urllib3_logger.add_memory_logger()                                      # add a memory logger
+        requests.get('https://www.google.com/aaa')                              # make a request to Google which uses urllib3
+        assert urllib3_logger.memory_handler_messages() == ['Starting new HTTPS connection (1): www.google.com:443',
+                                                            'https://www.google.com:443 "GET /aaa HTTP/1.1" 404 1564']
+
+    def test_debug__info__warning__error__critical(self):
+        self.logger.add_memory_logger()
+        assert self.logger.debug   ('debug message'    ) is True
+        assert self.logger.info    ('info message'     ) is True
+        assert self.logger.warning ('warning message'  ) is True
+        assert self.logger.error   ('error message'    ) is True
+        assert self.logger.critical('critical message' ) is True
+        assert self.logger.memory_handler_messages() == [ 'debug message'   ,
+                                                          'info message'    ,
+                                                          'warning message' ,
+                                                          'error message'   ,
+                                                          'critical message']
+        log_messages = self.logger.memory_handler_logs(group_by='levelname')
+
+        assert list_set(log_messages) == ['CRITICAL', 'DEBUG', 'ERROR', 'INFO', 'WARNING']
+        critical_logs = log_messages.get('CRITICAL')
+        debug_logs    = log_messages.get('DEBUG')
+        error_logs    = log_messages.get('ERROR')
+        info_logs     = log_messages.get('INFO')
+        warning_logs  = log_messages.get('WARNING')
+
+        def assert_log(log_entries, levelname, levelno, message):
+            assert size(log_entries) == 1
+            log_entry = log_entries.pop()
+            assert log_entry.get('levelname') == levelname
+            assert log_entry.get('levelno'  ) == levelno
+            assert log_entry.get('message'  ) == message
+            assert list_set(log_entry) == [ 'args',  'created',  'exc_info',  'exc_text',  'filename',  'funcName',  'levelname',  'levelno',  'lineno',  'message',  'module',  'msecs',  'msg',  'name',  'pathname',  'process',  'processName',  'relativeCreated',  'stack_info',  'thread',  'threadName' ]
+
+        assert_log(critical_logs, 'CRITICAL', 50, 'critical message')
+        assert_log(debug_logs   , 'DEBUG'   , 10, 'debug message'   )
+        assert_log(error_logs   , 'ERROR'   , 40, 'error message'   )
+        assert_log(info_logs    , 'INFO'    , 20, 'info message'    )
+        assert_log(warning_logs , 'WARNING' , 30, 'warning message' )
+
+    def test_exception(self):
+        self.logger.add_memory_logger()
+        assert self.logger.memory_handler_exceptions() == {}
+        self.logger.exception()                                     # log with no params outside an exception
+        log_exception = self.logger.memory_handler_logs().pop()
+        assert log_exception.get('exc_info' ) == (None, None, None)
+        assert log_exception.get('exc_text' ) == 'NoneType: None'
+        assert log_exception.get('levelname') =='ERROR'
+        assert log_exception.get('message'  ) == ''
+        try:
+            40 / 0
+        except Exception as error:
+            self.logger.logger.exception(error)
+
+        log_exception = self.logger.memory_handler_logs().pop()
+
+        assert str(log_exception.get('exc_info')[0]) == "<class 'ZeroDivisionError'>"
+        assert 'ZeroDivisionError: division by zero' in log_exception.get('exc_text')
+        assert log_exception.get('message') == 'division by zero'
