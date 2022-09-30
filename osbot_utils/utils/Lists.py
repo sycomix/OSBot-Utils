15204f603996c22c72f32477d7141cebd4fcacc8
Dinis Cruz <dinis.cruz@owasp.org>
2022-09-30 14:00:53 +0100

    Added Tuple support to Lists

--- osbot_utils/utils/Lists.py
+++ osbot_utils/utils/Lists.py
@@ -23,7 +23,26 @@ class Lists:
     def empty(list):
         return not Lists.not_empty(list)
 
-list_chunks    = Lists.chunks
-list_empty     = Lists.empty
-list_first     = Lists.first
-list_not_empty = Lists.not_empty
+    def tuple_to_list(target:tuple):
+            if type(target) is tuple:
+                return list(target)
+
+    def tuple_replace_position(target:tuple, position,value):
+        tuple_as_list = tuple_to_list(target)
+        if len(tuple_as_list) > position:
+            tuple_as_list[position] = value
+        list_as_tuple = list_to_tuple(tuple_as_list)
+        return list_as_tuple
+
+    def list_to_tuple(target: list):
+        if type(target) is list:
+            return tuple(target)
+
+list_chunks            = Lists.chunks
+list_empty             = Lists.empty
+list_first             = Lists.first
+list_not_empty         = Lists.not_empty
+list_to_tuple          = Lists.list_to_tuple
+
+tuple_to_list          = Lists.tuple_to_list
+tuple_replace_position = Lists.tuple_replace_position
