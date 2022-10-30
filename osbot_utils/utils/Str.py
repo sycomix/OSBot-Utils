0c2c4345621bbf779e89f72ceba85765ad9f80ad
Dinis Cruz <dinis.cruz@owasp.org>
2022-10-30 16:32:25 +0000

    added str_join helper method

--- osbot_utils/utils/Str.py
+++ osbot_utils/utils/Str.py
@@ -6,6 +6,8 @@ def html_escape(value):
 def html_unescape(value):
     return unescape(value)
 
+def str_join(delimiter, values):
+    return delimiter.join(values)
 
 html_encode = html_escape
 html_decode = html_unescape
