3d5d96671995624dce2ff2fda4b8fb854dcfce71
Dinis Cruz <dinis.cruz@owasp.org>
2023-01-30 04:11:50 +0000

    added str_safe method

--- osbot_utils/utils/Str.py
+++ osbot_utils/utils/Str.py
@@ -1,5 +1,8 @@
 from html import escape, unescape
 
+from osbot_utils.utils.Files import safe_file_name
+
+
 def html_escape(value):
     return escape(value)
 
@@ -8,6 +11,9 @@ def html_unescape(value):
 
 def str_join(delimiter, values):
     return delimiter.join(values)
+
+def str_safe(value):
+    return safe_file_name(value)
 
 html_encode = html_escape
 html_decode = html_unescape
