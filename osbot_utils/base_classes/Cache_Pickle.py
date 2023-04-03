ba95bab88dad912bc32a8513395ae84178088e9b
Dinis Cruz <dinis.cruz@owasp.org>
2023-04-03 18:44:03 +0100

    Major improvement to Cache_Pickle (now it supports cache files based on the hooked method params)

--- osbot_utils/base_classes/Cache_Pickle.py
+++ osbot_utils/base_classes/Cache_Pickle.py
@@ -2,9 +2,11 @@ import os
 from functools import wraps
 
 from osbot_utils.utils.Files import path_combine, folder_create, temp_folder_current, file_exists, pickle_load_from_file, pickle_save_to_file
+from osbot_utils.utils.Misc import str_md5
 from osbot_utils.utils.Python_Logger import logger_info
 
 FOLDER_CACHE_ROOT_FOLDER = '_cache_pickle'
+SUPPORTED_PARAMS_TYPES   = [int, float, bytearray, bytes, bool, complex, str]
 
 class Cache_Pickle:
 
@@ -32,28 +34,36 @@ class Cache_Pickle:
         return self
 
     def cache_data(self, func):
+
         @wraps(func)
         def wrapper(*args, **kwargs):
-            caller_name = func.__name__
+            if type(func.__self__) != type(self):       # if the parent type of the function is not self, then just execute it (this happens if a function is set as a variable)
+                return func(*args, **kwargs)
 
-            path_file = path_combine(self.cache_path(), f'{caller_name}.pickle')
+            # first check the params that are specific for this cache method (and cannot be propagated)
             if 'reload_cache' in kwargs:                                        # if the reload parameter is set to True
                 reload_cache = kwargs['reload_cache']                           # set reload to the value provided
                 del kwargs['reload_cache']                                      # remove the reload parameter from the kwargs
             else:
                 reload_cache = False                                            # otherwise set reload to False
 
-            if 'use_cache' in kwargs:
+            if 'use_cache' in kwargs:                                           # see if we want to disable cache
                 use_cache = kwargs['use_cache']
                 del kwargs['use_cache']
             else:
                 use_cache = True
+            # after processing these extra params we can resolve the file name and check if it exists
+            cache_file_name = self.cache_resolve_file_name(func, args, kwargs)
+
+            # path_file = path_combine(self.cache_path(), f'{caller_name}.pickle')
+            path_file = path_combine(self.cache_path(), cache_file_name)
 
             if use_cache is True and reload_cache is False and file_exists(path_file):
                 return pickle_load_from_file(path_file)
             else:
                 data = func(*args, **kwargs)
                 if data and use_cache is True:
+                    caller_name = func.__name__
                     self.log_info(f"Saving cache file data for: {caller_name}")
                     pickle_save_to_file(data, path_file)
                 return data
@@ -71,3 +81,31 @@ class Cache_Pickle:
     def cache_setup(self):
         folder_create(self.cache_path())
         return self
+
+    def cache_kwargs_to_str(self, kwargs):
+        kwargs_values_as_str = ''
+        if kwargs:
+            for key, value in kwargs.items():
+                if type(value) in SUPPORTED_PARAMS_TYPES:
+                    kwargs_values_as_str += f'{key}:{value}|'
+        return kwargs_values_as_str
+
+    def cache_args_to_str(self, args):
+        args_values_as_str = ''
+        if args:
+            for arg in args:
+                if type(arg) in SUPPORTED_PARAMS_TYPES:
+                    args_values_as_str += str(arg)
+        return args_values_as_str
+
+    def cache_resolve_file_name(self, function, args=None, kwargs=None):
+        key_name               = function.__name__
+        args_md5               = ''
+        kwargs_md5             = ''
+        args_values_as_str     = self.cache_args_to_str(args)
+        kwargs_values_as_str   = self.cache_kwargs_to_str(kwargs)
+        if args_values_as_str  : args_md5   = '_' + str_md5(args_values_as_str  )
+        if kwargs_values_as_str: kwargs_md5 = '_' + str_md5(kwargs_values_as_str)
+        cache_file_name        = f'{key_name}{args_md5}{kwargs_md5}'
+        cache_file_name       += '.pickle'
+        return cache_file_name
