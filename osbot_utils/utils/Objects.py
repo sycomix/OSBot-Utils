12dc6c5e7a9cb9538b5787700192959b917463f5
Dinis Cruz <dinis.cruz@owasp.org>
2023-10-31 17:44:28 +0000

    Improved obj_base_classes methods

--- osbot_utils/utils/Objects.py
+++ osbot_utils/utils/Objects.py
@@ -1,18 +1,19 @@
 # todo add tests
-def obj_all_base_classes(obj):
-    return type_all_base_classes(type(obj))
+def obj_base_classes(obj):
+    return type_base_classes(type(obj))
 
-def type_all_base_classes(cls):
+def type_base_classes(cls):
     base_classes = cls.__bases__
     all_base_classes = list(base_classes)
     for base in base_classes:
-        all_base_classes.extend(type_all_base_classes(base))
+        all_base_classes.extend(type_base_classes(base))
     return all_base_classes
 
-#todo: see if this is not a better implementation of this method
-# def type_all_base_classes(cls):
-#     base_classes = cls.__bases__
-#     all_base_classes = set(base_classes)
-#     for base in base_classes:
-#         all_base_classes.update(type_all_base_classes(base))
-#     return all_base_classes
+def obj_base_classes_names(obj, show_module=False):
+    names = []
+    for base in obj_base_classes(obj):
+        if show_module:
+            names.append(base.__module__ + '.' + base.__name__)
+        else:
+            names.append(base.__name__)
+    return names
